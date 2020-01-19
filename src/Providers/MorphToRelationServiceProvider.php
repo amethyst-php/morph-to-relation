6fc4a131aced79a58d785e8d96aebf74681cbf7f
railken <railken@protonmail.ch>
2020-01-19 18:29:24 +0100

    fix namespaces

--- src/Providers/MorphToRelationServiceProvider.php
+++ src/Providers/MorphToRelationServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace Amethyst\Providers;
 
-use Amethyst\Common\CommonServiceProvider;
+use Amethyst\Core\Providers\CommonServiceProvider;
 use Amethyst\Models\MorphToRelation;
 use Amethyst\Observers\MorphToRelationObserver;
 use Illuminate\Support\Facades\Config;
